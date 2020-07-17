c7b7dbb1670fa255c1747eaab66166fb9f45d943
Shubhavi Arya <aryax014@umn.edu>
2020-07-17 17:09:45 -0500

    psiturk

--- src/lib/markup/fixation.js
+++
@@ -1,2 +0,0 @@
-export const fixationHTML =
-  '<div class="effort-container"><div id="fixation-dot" class="color-white"> </div></div>';
--- src/lib/markup/stimuli.js
+++
@@ -1,6 +0,0 @@
-const baseStimulus = (element, prompt = false) => {
-  const class_ = prompt ? "main-prompt" : "effort-container";
-  return `<div class=${class_}>${element}</div>`;
-};
-
-export { baseStimulus };
